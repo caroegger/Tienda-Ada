95beebf7abd0fa27f4b2cf58e6ee248efd15e6dd
carolaegger <carolaeggr@gmail.com>
2020-10-16 03:57:31 -0300

    agregue modal checkout

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 
+//ABRIR Y CERRAR CARRITO
 const botonAbrirCarrito = document.getElementById("abrir-carrito")
 const botonCerrarCarrito = document.getElementById("cerrar-carrito")
 const carrito = document.getElementById("carrito")
-const overlay = document.getElementById("overlay")
+const overlayCarrito = document.getElementById("overlay-carrito")
+//ABRIR Y CERRAR CHECKOUT
+const botonAbrirCheckout = document.getElementById("boton-compra")
+const botonSalirCheckout = document.getElementById("boton-vaciar")
+const checkout = document.getElementById("checkout")
+const overlayCheckout = document.getElementById("overlay-checkout")
+//VACIAR CARRITO
+const abrirModalVaciar = document.getElementById("boton-vaciar")
+const cancelarVaciar = document.getElementById("boton-compra")
+const modalVaciar = document.getElementById("modal-vaciar")
+const overlayVaciar = document.getElementById("overlay-vaciar")
 
+//ABRIR Y CERRAR CARRITO
 botonAbrirCarrito.onclick = () => {
-    overlay.classList.remove("ocultar")
+    overlayCarrito.classList.remove("ocultar")
+    document.body.classList.add("no-scroll");
 }
 botonCerrarCarrito.onclick = () => {
-    overlay.classList.add("ocultar")
+    overlayCarrito.classList.add("ocultar")
+    document.body.classList.remove("no-scroll");
 }
+
+//ABRIR Y CERRAR CHECKOUT
+botonAbrirCheckout.onclick = () => {
+    overlayCheckout.classList.remove("ocultar")
+    document.body.classList.add("no-scroll");
+}
+botonSalirCheckout.onclick = () => {
+    overlayCheckout.classList.add("ocultar")
+    document.body.classList.remove("no-scroll");
+}
+
+// //VACIAR CARRITO
+// abrirModalVaciar.onclick = () => {
+//     overlayVaciar.classList.remove("ocultar")
+//     document.body.classList.add("no-scroll");
+// }
+// cancelarVaciar.onclick = () => {
+//     overlayVaciar.classList.add("ocultar")
+//     document.body.classList.remove("no-scroll");
+// }
